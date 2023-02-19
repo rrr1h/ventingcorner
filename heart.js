f7bdc85a1c2b739ce69dc2fdb84f3b3e4b8f75b6
rrr1h <125781669+rrr1h@users.noreply.github.com>
2023-02-19 05:11:49 +0100

    Add files via upload

---
+++ heart.js
@@ -0,0 +1,10 @@
+document.getElementById("better-btn").addEventListener("click", redirectToBetterPage);
+
+function redirectToBetterPage() {
+  window.location.href = "https://www.youtube.com/watch?v=aFEIqn0r_lQ";
+}
+
+
+function redirectToSadPage() {
+  window.location.href = "https://www.youtube.com/watch?v=SUelbSa-OkA";
+}
